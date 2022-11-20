18de31330b12d9cf7b1d41d72b8e46da9859d038
Junxiang <69403156+tobyelephant@users.noreply.github.com>
2022-11-20 11:58:19 -0500

    Project 8 upload

--- ScrollBar.js
+++ ScrollBar.js
@@ -2,7 +2,7 @@ var height = $('.header').height();
 
 $(window).scroll(function(){
     if($(this).scrollTop() > height){
-        $('.nav').addClass('fixed');
+        $('.nav').addClass('fixed',{duration:200,effect:'blind'});
     }else{
         $('.nav').removeClass('fixed');
     }

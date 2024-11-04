85685824627395a7c8555e4452e9084e155a5a55
Junxiang <69403156+tobyelephant@users.noreply.github.com>
2024-11-04 14:56:01 -0500

    Delete ScrollBar.js

--- ScrollBar.js
+++
@@ -1,9 +0,0 @@
-var height = $('.header').height();
-
-$(window).scroll(function(){
-    if($(this).scrollTop() > height){
-        $('.nav').addClass('fixed',{duration:200,effect:'blind'});
-    }else{
-        $('.nav').removeClass('fixed');
-    }
-});
